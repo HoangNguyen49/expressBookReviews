148f73c624f6e85b821162b372ddecc7b7f5c5ad
Hoang HL <hoangmh49@gmail.com>
2025-05-03 14:28:21 +0700

    update get book by title and get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -49,14 +49,37 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+
+  const bookKeys = Object.keys(books);
+
+  const booksByTitle = [];
+
+  bookKeys.forEach((key) => {
+    // Check if the book title matches the requested title
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      booksByTitle.push(books[key]);
+    }
+  });
+
+  if(booksByTitle.length > 0) {
+    return res.status(200).send(JSON.stringify(booksByTitle, null, 2));
+  }
+  else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if(book){
+    return res.status(200).send(JSON.stringify(book.reviews, null, 2));
+  }else{
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
